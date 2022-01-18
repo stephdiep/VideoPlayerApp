03ec3f9017333e5826dc08e47056df41d4cef873
stephdiep <stephanie.diep002@gmail.com>
2022-01-18 10:25:11 -0500

    Initial Commit

---
+++ VideoFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VideoFinder
+//
+//  Created by Stephanie Diep on 2022-01-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VideoFinder/VideoFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoFinderApp.swift
+//  VideoFinder
+//
+//  Created by Stephanie Diep on 2022-01-18.
+//
+
+import SwiftUI
+
+@main
+struct VideoFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
